d349a9bc617df89804dfbcfe156dce1b244587bd
gamalielhere <gamalielgad1234@gmail.com>
2019-10-21 14:26:35 -0700

    Update config to correct value

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   presets: [
     ["@babel/preset-env",{
-      useBuiltIns: true,
+      useBuiltIns: "entry",
       "corejs": "core-js@3"
     }],
     ['@vue/app', { useBuiltIns: "entry" } ]
